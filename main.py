2edab58305985adf3f01343881d1a8958d43cae2
chwang <exactdiagonalization@gmail.com>
2018-05-15 19:24:16 +0800

    add new pages

--- main.py
+++ main.py
@@ -9,32 +9,64 @@ from flask import request
 import json
 
 app = Flask(__name__)
-json_file = 'data/3c.json'
+# json_file = 'data/3c.json'
 # json_file = 'data/makeup.json'
-articleList = []
-with open(json_file, 'r') as f:
-    articles = json.load(f)
-    for a in articles: articleList.append([a['id'], a['title']])
+# with open(json_file, 'r') as f:
+#     articles = json.load(f)
+#     for a in articles: articleList.append([a['id'], a['title']])
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    with open(json_file, 'r') as f: articles = json.load(f)
+    return render_template('home.html')
+
+@app.route('/tech', methods=['GET', 'POST'])
+def tech():
+    json_file = 'data/3c.json'
+    articleList = []
+    with open(json_file, 'r') as f: 
+        articles = json.load(f)
+        for a in articles: articleList.append([a['id'], a['title']])
     index = request.args.get('index')
-    print('index:', index)
     if not index: index = 'l206396230'  # 3c
-    # if not index: index = 'v221808882'  # makeup
+    articleType = 'tech'
     for a in articles:
         if a['id']==index:
             articleName = a['title']
             articleLink = a['link']
             content = a['content']
             break
-    return render_template('index.html', article=content, articleIndex=index, articleName=articleName, articleLink=articleLink, articleList=articleList)
+    return render_template('index.html', article=content, articleIndex=index, articleName=articleName, 
+    articleLink=articleLink, articleList=articleList, articleType = articleType)
+
+@app.route('/makeup', methods=['GET', 'POST'])
+def makeup():
+    json_file = 'data/makeup.json'
+    articleList = []
+    with open(json_file, 'r') as f: 
+        articles = json.load(f)
+        for a in articles: articleList.append([a['id'], a['title']])
+    index = request.args.get('index')
+    print('index:', index)
+    if not index: index = 'v221808882'  # makeup
+    articleType = 'makeup'
+    for a in articles:
+        if a['id']==index:
+            articleName = a['title']
+            articleLink = a['link']
+            content = a['content']
+            break
+    return render_template('index.html', article=content, articleIndex=index, articleName=articleName, 
+    articleLink=articleLink, articleList=articleList, articleType = articleType)
+
 
 @app.route('/save', methods=['POST'])
 def save():
     try:
         content = request.get_json()
+        if content['articleType'] == 'tech':
+            json_file = 'data/3c.json'
+        elif content['articleType'] == 'makeup':
+            json_file = 'data/makeup.json'
         with open(json_file, 'r') as f: data = json.load(f)
         for d in data:
             if d['id']==content['index']: d['content']=content['data']
