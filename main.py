1f9aaf3404c55084c6237b08ab71334c6645d83e
Kuang Kao <kenkao70508@gmail.com>
2018-06-05 19:53:27 +0800

    add port

--- main.py
+++ main.py
@@ -90,5 +90,5 @@ def save():
         return jsonify(message=str(e)), 500
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='140.112.90.203', port=5511)
-    app.run(debug=True)
+    app.run(debug=True, host='140.112.90.203', port=5511)
+    # app.run(debug=True)
