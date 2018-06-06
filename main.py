e7105cfbc0ecdb5f92c39024cb895be7ad54c2b3
chwang <exactdiagonalization@gmail.com>
2018-06-06 18:55:53 +0800

    new port

--- main.py
+++ main.py
@@ -169,5 +169,5 @@ def getJsonLoc(category):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='140.112.90.203', port=5511)
+    app.run(debug=True, host='140.112.90.203', port=5789)
     # app.run(debug=True)
